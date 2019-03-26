338888119b6d6112c47c9de7d1229411663e6a44
张亚浩 <zhangyahao897@pingan.com.cn>
2019-03-26 18:05:10 +0800

    add source files

---
+++ ModuleA/ModuleA/ModuleA_FirstViewController.h
@@ -0,0 +1,17 @@
+//
+//  ModuleA_FirstViewController.h
+//  ModuleA
+//
+//  Created by 张亚浩 on 2019/3/26.
+//  Copyright © 2019 Ace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ModuleA_FirstViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ModuleA/ModuleA/ModuleA_FirstViewController.m
@@ -0,0 +1,32 @@
+//
+//  ModuleA_FirstViewController.m
+//  ModuleA
+//
+//  Created by 张亚浩 on 2019/3/26.
+//  Copyright © 2019 Ace. All rights reserved.
+//
+
+#import "ModuleA_FirstViewController.h"
+
+@interface ModuleA_FirstViewController ()
+
+@end
+
+@implementation ModuleA_FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
