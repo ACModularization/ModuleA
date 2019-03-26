4f94472b81148a0da65b3281ac4373e76fbaf168
张亚浩 <zhangyahao897@pingan.com.cn>
2019-03-26 17:40:44 +0800

    config

---
+++ ModuleA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModuleA
+//
+//  Created by 张亚浩 on 2019/3/25.
+//  Copyright © 2019 Ace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModuleA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleA
+//
+//  Created by 张亚浩 on 2019/3/25.
+//  Copyright © 2019 Ace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
